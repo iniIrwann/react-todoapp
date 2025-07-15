4e7b35fc6e6e0a2583880c754db7b81bff9b476e
Irwan Akhmad <irwanakhmadzainni@gmail.com>
2025-07-15 09:30:06 +0700

    fix: perbaiki nama file komponen companyLogo and cardIllustration

--- src/assets/data/companyLogo.jsx
+++ src/assets/data/companyLogo.jsx
@@ -1,9 +1,9 @@
 import companyLogo from "@assets/CompanyLogo.png";
-import companyLogo1 from "@assets/CompanyLogo(1).png";
-import companyLogo2 from "@assets/CompanyLogo(2).png";
-import companyLogo3 from "@assets/CompanyLogo(3).png";
-import companyLogo4 from "@assets/CompanyLogo(4).png";
-import companyLogo5 from "@assets/CompanyLogo(5).png";
+import companyLogo1 from "@assets/CompanyLogo-1.png";
+import companyLogo2 from "@assets/CompanyLogo-2.png";
+import companyLogo3 from "@assets/CompanyLogo-3.png";
+import companyLogo4 from "@assets/CompanyLogo-4.png";
+import companyLogo5 from "@assets/CompanyLogo-5.png";
 
 export const companyLogos = [
   companyLogo,
