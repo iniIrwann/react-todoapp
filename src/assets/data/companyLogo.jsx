ae5a2a559e71c9016615a37bd4e6d37721d261ca
Irwan Akhmad <irwanakhmadzainni@gmail.com>
2025-07-15 09:22:03 +0700

    fix: perbaiki path komponen companyLogo and cardIllustration

--- src/assets/data/companyLogo.jsx
+++ src/assets/data/companyLogo.jsx
@@ -1,9 +1,9 @@
-import companyLogo from "@assets/Company logo.png";
-import companyLogo1 from "@assets/Company logo (1).png";
-import companyLogo2 from "@assets/Company logo (2).png";
-import companyLogo3 from "@assets/Company logo (3).png";
-import companyLogo4 from "@assets/Company logo (4).png";
-import companyLogo5 from "@assets/Company logo (5).png";
+import companyLogo from "@assets/CompanyLogo.png";
+import companyLogo1 from "@assets/CompanyLogo(1).png";
+import companyLogo2 from "@assets/CompanyLogo(2).png";
+import companyLogo3 from "@assets/CompanyLogo(3).png";
+import companyLogo4 from "@assets/CompanyLogo(4).png";
+import companyLogo5 from "@assets/CompanyLogo(5).png";
 
 export const companyLogos = [
   companyLogo,
